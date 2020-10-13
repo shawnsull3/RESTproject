217fac0c26f653c020ab57237b90cf76df97eade
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-13 13:23:20 -0600

    Link api url

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UsersSerializer
+from .models import Users
+
+# ModelViewSet handles GET & POST routes for Users
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all().order_by('username')
+    serializer_class = UsersSerializer
