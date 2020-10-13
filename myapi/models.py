088cd5c8a7f2d5a6ae21e4f5c200701bcff27bd4
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-13 12:27:48 -0600

    Create a basic Users model. Migrated model into project

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=50)
+    fullName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=60)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.username
